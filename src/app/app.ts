40db6abad6589ae99b2b642c2c98871fa67190a9
Mateo Paez <mpaez94144@gmail.com>
2026-05-14 09:29:12 -0500

    Fin de la Practica 02

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeader } from './components/app-header/app-header';
+import { AppHeroComponent } from './components/app-hero/app-hero';
+import { AppFooterComponent } from './components/app-footer/app-footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppHeader],
+  imports: [RouterOutlet, AppHeader, AppHeroComponent, AppFooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
