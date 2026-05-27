28f0fe3748740a7ae7ab283f0e382c29c269c0ad
Mateo Paez <mpaez94144@gmail.com>
2026-05-27 15:40:49 -0500

    UPDATE: Practica 08-09: Auth Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,40 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+
+import { provideHttpClient } from '@angular/common/http';
+
+// Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+// Importas el objeto que acabamos de crear
+import { environment } from '../environments/environment';
+
+const firebase = {
+    apiKey: "AIzaSyDTnjw9_xepjp1B186PLknmy9zsZtepI1w",
+    authDomain: "ppw-practicaangular.firebaseapp.com",
+    projectId: "ppw-practicaangular",
+    storageBucket: "ppw-practicaangular.firebasestorage.app",
+    messagingSenderId: "428754213972",
+    appId: "1:428754213972:web:b5f11837efab7c65db21c1",
+    measurementId: "G-PDE24YKRCR"
+  }
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+
+    provideHttpClient(),
+
+    // Usas la referencia del environment
+    provideFirebaseApp(() => initializeApp(firebase)),
+
+    provideAuth(() => getAuth()),
+
+    provideFirestore(() => getFirestore()),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { ProjectConfigPage } from './features/project-config-page/project-config
 import { UiComponentsPage } from './features/ui-components-page/ui-components-page';
 import { SimpsonsPage } from './features/simpsons/pages/simpsons-page/simpsons-page';
 import { SimpsonDetailPage } from './features/simpsons/pages/simpson-detail-page/simpson-detail-page';
+import { AuthPage } from './features/auth/pages/auth-page/auth-page';
 
 export const routes: Routes = [
     {path: '', component: HomePage},
@@ -21,7 +22,7 @@ export const routes: Routes = [
     {path: 'ui-components', component: UiComponentsPage},
     {path: 'simpsons', component: SimpsonsPage},
     {path: 'simpsons/:id', component: SimpsonDetailPage},
-
+    {path: 'auth', component: AuthPage},
     //redirect
     {path: '**', redirectTo: ''}
 ];
